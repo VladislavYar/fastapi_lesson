ce005e8c83f881b75a283b72a2a77fd3e46d6fb0
Vladislav Yaremenko <vlad.yaremenko.98@yandex.ru>
2024-06-06 18:42:15 +0300

    Create settings.

--- main.py
+++ main.py
@@ -1,11 +1,20 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 import uvicorn
 
 from views.calc import router
+from config import settings
 
 
 app = FastAPI()
 app.include_router(router, prefix="/calc")
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=settings.cors.allow_origins,
+    allow_credentials=settings.cors.allow_credentials,
+    allow_methods=settings.cors.allow_methods,
+    allow_headers=settings.cors.allow_headers,
+)
 
 
 @app.get("/hello")
@@ -25,4 +34,10 @@ def hi_by_path(name: str = "World!") -> dict[str, str]:
 
 
 if __name__ == '__main__':
-    uvicorn.run("main:app", reload=True)
+    print(settings.model_dump())
+    uvicorn.run(
+        "main:app",
+        host=settings.host,
+        port=settings.port,
+        reload=settings.reload,
+        )
